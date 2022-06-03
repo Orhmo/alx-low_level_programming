d2ddd4b5811413fe921f6bf4b025f6b1087656c9
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-03 23:33:03 +0100

    Teba

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,15 @@
-#incude <stdio.h>
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+char a;
+for (a = 'z'; a >= 'a' ; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
