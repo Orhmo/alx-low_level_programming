2915e7e98cea049f7201a8d72e0c59acb63704da
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-04 00:16:42 +0100

    Inheirt

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
-
 /**
  * main - main
  *
  * Return: 0
- *
  */
-
 int main(void)
 {
 int i;
 int j;
-  
 for (i = 10; i <= 19; i++)
 {
 for (j = 10; j <= 19; j++)
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
-	  
-if (i != 19 ||  j != 19)	    
+if (i != 19 ||  j != 19)
 {
 putchar(',');
-putchar(' ');  
-} 
-}  
+putchar(' ');
+}
+}
 }
 putchar('\n');
-return (0); 
+return (0);
 }
