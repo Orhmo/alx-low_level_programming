abefebff76ffc3d1ca6f486c0c806bfb0b866d2b
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-04 00:08:09 +0100

    Inventing

--- 0x01-variables_if_else_while/100-print_comb2.c
+++ 0x01-variables_if_else_while/100-print_comb2.c
@@ -1,55 +1,31 @@
 #include <stdio.h>
 
 /**
-
- * main - entry point
-
+ * main - main
  *
-
- * Return: alway returns 0
-
+ * Return: 0
  *
-
  */
 
 int main(void)
-  
 {
+int i;
+int j;
   
-  int i;
-  
-  int j;
-  
-
-  
-  for (i = 10; i <= 19; i++)
-    
-    {
-      
-      for (j = 10; j <= 19; j++)
-	
-	{
+for (i = 10; i <= 19; i++)
+{
+for (j = 10; j <= 19; j++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
 	  
-	  putchar((i % 10) + '0');
-	  
-	  putchar((j % 10) + '0');
-	  
-	  if (i != 19 ||  j != 19)
-	    
-	    {
-	      
-	      putchar(',');
-	      
-	      putchar(' ');
-	      
-	    }
-	  
-
-	  
-	}
-      
-    }
-  
-  putchar('\n');
-  return (0); 
+if (i != 19 ||  j != 19)	    
+{
+putchar(',');
+putchar(' ');  
+} 
+}  
+}
+putchar('\n');
+return (0); 
 }
