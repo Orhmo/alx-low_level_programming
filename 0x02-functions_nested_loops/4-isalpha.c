5a37aed341c984d660982409599e5a32d0e886a3
Valentina <oorhmo@gmail.com>
2022-06-08 16:25:31 -0700

    Alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - main function
+ *@c: ASCII character
+ *
+ *Return: 1 (if true)
+ *
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
