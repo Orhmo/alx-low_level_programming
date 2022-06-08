4f5feb7e279564917efacce81a77e5ccad5d0607
Valentina <oorhmo@gmail.com>
2022-06-08 16:41:56 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_sign - main
+ *
+ *@n: character
+ *
+ *Return: 1(positive), 0(zero), -1(negetive)
+ */
+int print_sign(int n)
+{
+  if (n > 0)
+    {
+      _putchar('+');
+      return (1);
+
+    }
+  if (n==0)
+    {
+      _putchar
