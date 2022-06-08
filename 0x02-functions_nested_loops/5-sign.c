ed5c20c4c344f03a73cca1ef4daa80e583d73577
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-09 00:44:01 +0100

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,16 @@
  */
 int print_sign(int n)
 {
-  if (n > 0)
-    {
-      _putchar('+');
-      return (1);
-
-    }
-  if (n==0)
-    {
-      _putchar
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n==0)
+{
+_putchar
+return (0);
+}
+_putchar('-');
+return (-1);
+}
