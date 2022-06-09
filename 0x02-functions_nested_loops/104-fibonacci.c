a2ce5436401cf569edb1826bee8780f63e00d48a
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-09 04:22:41 +0100

    Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,22 +6,22 @@
  */
 int main(void)
 {
-	int counter = 2;
+	int count = 2;
 
 	float x = 1;
-	float y = x + 1;
+	float y = 2;
 	float z = x + y;
 
 	printf("%.0f, ", x);
 	printf("%.0f, ", y);
-	while (counter < 98)
+	while (count < 98)
 	{
-		counter++;
+		count++;
 		printf("%.0f", z);
 		x = y;
 		y = z;
 		z = x + y;
-		if (counter < 98)
+		if (count < 98)
 		{
 			printf(", ");
 		}
