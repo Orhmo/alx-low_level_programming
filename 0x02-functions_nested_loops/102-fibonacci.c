99504452494f829056a44715e567015709e67cf8
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-09 03:59:04 +0100

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - rints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * main - prints the first 50 Fibonacci numbers
  *
  *Return: 0
  */
