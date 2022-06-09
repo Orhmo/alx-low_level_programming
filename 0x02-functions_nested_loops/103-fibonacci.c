6634885cf1e76f87242aecd036c0100ff7c86d5e
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-09 04:08:04 +0100

    Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,3 +4,23 @@
  *
  * Return : 0
  */
+int main(void)
+{
+	int count = 0;
+	long int i = 1;
+	long int j = i;
+	long int k = i + j;
+
+	while (k < 4000000)
+	{
+		if (k % 2 == 0)
+		{
+			count += k;
+		}
+		i = j;
+		j = k;
+		k = i + j;
+	}
+	printf("%d\n", count);
+	return (0);
+}
