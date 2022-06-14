7d974117ec7772178e6923b14ac4bf22c3923fde
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-14 20:41:39 +0100

    Strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,15 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
  * Return: The length of @str.
  */
 int _strlen(char *s)
 {
-int i;
+int c;
 
-for (i = 0; s[i] != '\0'; ++i)
-{
-continue;
-}
+for (c = 0; s[c] != '\0'; c++);
 
-return (i);
+return (c);
 }
