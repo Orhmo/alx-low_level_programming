e2b977b03293532ef26556195fbaa095b29712d8
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-06-15 21:19:46 +0100

    Cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ int i = 0;
 while (str[i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
-index++;
+i++;
 
 if (str[i - 1] == ' ' ||
 str[i - 1] == '\t' ||
@@ -27,8 +27,8 @@ str[i - 1] == '"' ||
 str[i - 1] == '(' ||
 str[i - 1] == ')' ||
 str[i - 1] == '{' ||
-str[i - 1] == '}' )
-str[index] -= 32;
+str[i - 1] == '}')
+str[i] -= 32;
 
-index++;
+i++;
 }
